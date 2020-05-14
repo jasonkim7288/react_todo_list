60763c12991638aa63f4623448e94052150b5851
jasonkim7288 <jason.kim7288@gmail.com>
2020-05-14 15:38:37 +1000

    Pass the argument from TodoItem to App

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import Todos from './components/Todos'
+
+import './App.css';
+
+export default class App extends Component {
+    state = {
+        todos: [
+            {
+                id: 1,
+                title: 'Take out the trash',
+                completed: false
+            },
+            {
+                id: 2,
+                title: 'Dinner with wife',
+                completed: false
+            },
+            {
+                id: 3,
+                title: 'Meeting with boss',
+                completed: false
+            }
+        ]
+    }
+
+    // Toggle Complete
+    markComplete = (id) => {
+        this.setState( { todos: this.state.todos.map(todo => {
+            if(todo.id === id) {
+                todo.completed = !todo.completed;
+            }
+            return todo;
+        })});
+        console.log(id);
+    }
+
+    render() {
+        return (
+            <div>
+                <Todos todos={this.state.todos} markComplete={this.markComplete}/>
+            </div>
+        )
+    }
+}
