2ec185be91b641b4beb86d38adefbfdf9b488de3
jasonkim7288 <jason.kim7288@gmail.com>
2020-05-14 22:43:15 +1000

    Updates

---
+++ precache-manifest.fbc5564b4c39a17d5e1d7ed4c35a106b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa7316352696673a742dc8358b8a0fe7",
+    "url": "/react_todo_list/index.html"
+  },
+  {
+    "revision": "b08f522106c4a1d7c04f",
+    "url": "/react_todo_list/static/css/main.32570bfc.chunk.css"
+  },
+  {
+    "revision": "9c4b08390a0c8660675f",
+    "url": "/react_todo_list/static/js/2.f482cc75.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_todo_list/static/js/2.f482cc75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b08f522106c4a1d7c04f",
+    "url": "/react_todo_list/static/js/main.59b485d2.chunk.js"
+  },
+  {
+    "revision": "81dd94f9ca26331c4e18",
+    "url": "/react_todo_list/static/js/runtime-main.f398c08d.js"
+  }
+]);
